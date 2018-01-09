6d2fd4b496674e2f77cce39c026e39d0a52a6639
bit-user <bit-user@DESKTOP-C1QR4BV>
2018-01-09 19:10:17 +0900

    4번완료

--- src/com/javaex/problem04/Bird.java
+++ src/com/javaex/problem04/Bird.java
@@ -2,6 +2,18 @@ package com.javaex.problem04;
 
 public abstract class Bird {
     private String name;
+    
+    public abstract void fly();
+    public abstract void sing();
+    public abstract void showName();
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 
 
