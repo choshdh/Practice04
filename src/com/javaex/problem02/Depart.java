738c08aaa92a6eb63d4aebc48db6036562d54618
bit-user <bit-user@DESKTOP-C1QR4BV>
2018-01-09 18:59:00 +0900

    수정

--- src/com/javaex/problem02/Depart.java
+++ src/com/javaex/problem02/Depart.java
@@ -3,5 +3,25 @@ package com.javaex.problem02;
 public class Depart extends Employee {
 
     //코드작성
+	private String department;
 
+	public Depart() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Depart(String name, int salary , String department) {
+		super(name, salary);
+		this.department = department;
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void getInformation() {
+		System.out.println("이름:" + super.getName() + " 연봉:" + super.getSalary() + " 부서 :" + department);
+	}
+	
+	
+
+	
 }
--- src/com/javaex/problem03/SmartPhone.java
+++ src/com/javaex/problem03/SmartPhone.java
@@ -3,14 +3,33 @@ package com.javaex.problem03;
 public class SmartPhone extends MusicPhone {
     
     public void execute(String str){
-        
+        if(str.equals("앱")) {
+        	playApp();
+        }else if(str.equals("음악")) {
+        	playMusic();
+        }else {
+        	super.execute(str);
+        }
         //코드작성
         
     }
- 
-    //메소드작성
+
     
     //메소드작성
+	@Override
+	protected void playMusic() {
+		// TODO Auto-generated method stub
+		System.out.println("다운로드해서 음악재생");
+	}
+ 
+    
+	//메소드작성
+	public void playApp() {
+		System.out.println("앱실행");
+	}
+    
+    
+    
     
     
     
