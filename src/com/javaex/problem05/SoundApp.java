466f2858f1eda3d01a97b62c887dfdb1a2a4cc20
bit-user <bit-user@DESKTOP-C1QR4BV>
2018-01-09 19:21:51 +0900

    5번 완료

--- src/com/javaex/problem05/SoundApp.java
+++ src/com/javaex/problem05/SoundApp.java
@@ -11,6 +11,15 @@ public class SoundApp {
 
     public static void printSound( Soundable soundable ) {
         //구현
+    	if(soundable instanceof Cat) {
+    		System.out.println(((Cat)soundable).sound());
+    	}else if(soundable instanceof Dog){
+    		System.out.println(((Dog)soundable).sound());
+    	}else if(soundable instanceof Sparrow) {
+    		System.out.println(((Sparrow)soundable).sound());
+    	}else if(soundable instanceof Duck){
+    		System.out.println(((Duck)soundable).sound());
+    	}
     }
     
 }
